fac9be70d57f33cb9e04c82b07658f3aa593a89c
amitkummer <amit.kummer@gmail.com>
2021-10-27 19:58:16 +0300

    Add weird endpoint `/people/id/tasks/`

--- manager/tasks/serializers.py
+++ manager/tasks/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 
 from tasks.models import Task, Person
 
-class TaskSerizlier(serializers.ModelSerializer):
+class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = '__all__'
--- manager/tasks/views.py
+++ manager/tasks/views.py
@@ -1,15 +1,33 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import status
 
 from tasks.models import Task, Person
-from tasks.serializers import TaskSerizlier, PersonSerializer
-
-# Create your views here.
+from tasks.serializers import TaskSerializer, PersonSerializer
 
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
-    serializer_class = TaskSerizlier
+    serializer_class = TaskSerializer
 
 class PersonViewSet(viewsets.ModelViewSet):
     queryset = Person.objects.all()
     serializer_class = PersonSerializer 
+
+    @action(detail=True, methods=['get', 'post'])
+    def tasks(self, request, pk):
+        if request.method == 'GET':
+            person_tasks = Task.objects.filter(ownerId=pk)
+            if person_tasks.count() < 1:
+                return Response(data="{'Tasks not found'}", status=status.HTTP_404_NOT_FOUND)
+            serializer = TaskSerializer(person_tasks, many=True)
+            return Response(serializer.data)
+        else:
+            serializer = TaskSerializer(data=request.data)
+            if serializer.is_valid():
+                task = Task(serializer.data)
+                task.save()
+                return Response({'status': 'created'})
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
